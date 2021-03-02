00bf4e084fc53365740f26fe7238f4b071aa76ba
Maria Sargent <maria.marshall.83@gmail.com>
2021-03-01 18:18:38 -0600

    Added service-worker file. User input is saved when entered offline and uploaded when the user regains connection.

--- README.md
+++ README.md
@@ -15,15 +15,6 @@ When brought back online:
 
   * Offline entries should be added to tracker.
 
-## User Story
-AS AN avid traveller
-I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
-SO THAT my account balance is accurate when I am traveling
-
-## Business Context
-
-Giving users a fast and easy way to track their money is important, but allowing them to access that information anytime is even more important. Having offline functionality is paramount to our applications success.
-
 
 ## Acceptance Criteria
 GIVEN a user is on Budget App without an internet connection
--- public/index.js
+++ public/index.js
@@ -113,35 +113,35 @@ function sendTransaction(isAdding) {
   populateTotal();
   
   // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
-    body: JSON.stringify(transaction),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
+  // fetch("/api/transaction", {
+  //   method: "POST",
+  //   body: JSON.stringify(transaction),
+  //   headers: {
+  //     Accept: "application/json, text/plain, */*",
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  // .then(response => {    
+  //   return response.json();
+  // })
+  // .then(data => {
+  //   if (data.errors) {
+  //     errorEl.textContent = "Missing Information";
+  //   }
+  //   else {
+  //     // clear form
+  //     nameEl.value = "";
+  //     amountEl.value = "";
+  //   }
+  // })
+  // .catch(err => {
+  //   // fetch failed, so save in indexed db
+  //   // saveRecord(transaction);
 
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+  //   // clear form
+  //   nameEl.value = "";
+  //   amountEl.value = "";
+  // });
 }
 
 document.querySelector("#add-btn").onclick = function() {
